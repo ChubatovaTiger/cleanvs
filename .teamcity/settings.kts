440f450ff96370c70eeb73828ddf8a74c333104a
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-14 08:34:56 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 /*
 The settings script is an entry point for defining a TeamCity
